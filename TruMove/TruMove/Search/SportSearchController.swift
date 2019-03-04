b70ad0b923eb2f8d18cda19b8d5440271a758104
Zhixuan <wangzx16e@gmail.com>
2019-03-03 23:16:33 -0500

    Add files via upload

---
+++ TruMove/TruMove/Search/SportSearchController.swift
@@ -0,0 +1,106 @@
+//
+//  SportSearchController.swift
+//  groupproject
+//
+//  Created by Ellen Wang on 2019/3/1.
+//  Copyright © 2019 ellen. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SportSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
+
+    // MARK: SET UP SEARCHBAR
+    lazy var searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.placeholder = "Enter sport name"
+        sb.barTintColor = .gray
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = UIColor.rgb(red: 230, green: 230, blue: 230)
+        sb.delegate = self
+        return sb
+    }()
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchText.isEmpty {
+            filteredSports = Sports
+        } else {
+            filteredSports = self.Sports.filter { (item) -> Bool in
+                return item.lowercased().contains(searchText.lowercased())
+                }
+            }
+        
+        self.collectionView?.reloadData()
+    }
+    
+    let cellId = "cellId"
+    
+
+    //MARK: VIEW DID LOAD
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .white
+        
+        navigationController?.navigationBar.addSubview(searchBar)
+        
+        let navBar = navigationController?.navigationBar
+        
+    
+        searchBar.anchor(top: navBar?.topAnchor, left: navBar?.leftAnchor, bottom: navBar?.bottomAnchor, right: navBar?.rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: 0, height: 0)
+        
+        collectionView?.register(SportSearchCell.self, forCellWithReuseIdentifier: cellId)
+        
+        collectionView?.alwaysBounceVertical = true
+        collectionView?.keyboardDismissMode = .onDrag
+        fetchSports()
+        
+    }
+    
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        searchBar.isHidden = false
+    }
+    
+    // MARK: SET UP COLLECTION VIEW
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        searchBar.isHidden = true
+        searchBar.resignFirstResponder()
+        
+
+        let sportTrainController = SportIntroController()
+        
+        navigationController?.pushViewController(sportTrainController, animated: true)
+        
+    }
+
+    // MARK: SHOW ALL SPORTS LIST
+    var Sports = ["WEIGHTLIFTING", "BASKETBALL"]
+    var filteredSports = [" "]
+    
+    
+    fileprivate func fetchSports(){
+        self.filteredSports = self.Sports
+        self.collectionView?.reloadData()
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filteredSports.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SportSearchCell
+        
+        let imagename = filteredSports[indexPath.item] + ".png"
+        cell.sportImageView.image = UIImage(named: imagename)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 180)
+    }
+
+
+}
